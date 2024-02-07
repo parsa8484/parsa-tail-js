d0706f8c6951f4fb5bb0d61a41d93b4bb8d8012f
parsa8484 <pabedinejad@gmail.com>
2024-02-07 16:18:01 +0330

    start

---
+++ js/script.js
@@ -0,0 +1,55 @@
+    var username = "Tar"
+// var omg = prompt("لطفا نام و نام خانوادگی خود را وارد کنید :")
+// alert( " نام شما : "  + omm )
+// alert(typeof Number(omg))
+// alert(typeof +omg)
+
+                                    // _____finish part1_____
+
+// var x = prompt(" لطفا x را وارد کنید : ")
+// var y = prompt("لطفا y را وارد کنید :")
+
+// var z = (x ** y) 
+// alert(z)
+
+// or
+
+//  alert(x ** y)
+
+                                    // ______finish part2______
+
+// var x = 4
+// var y = 2
+// var z = "2"
+// var f = "parsa"
+
+// alert(isNaN(y + f) )  
+// alert(x == y)    =>      مقایسه متغیر ها با جواب درست و غلط    
+//  alert(x < y)    =>      مقایسه متغیر ها با جواب درست و غلط  
+// alert(y == z)    =>   2 مساوی فقط مقادیر داخل رو مقایسه میکنه 
+// alert(y === z)   =>   3 مساوی ذیتا تایپ ها رو هم بررسی میکنه
+
+                                     //______finish part3_______
+
+//  var x = prompt(" لطفا x را وارد کنید : ")
+//  var y = prompt("لطفا y را وارد کنید :")
+//  var z = prompt(" لطفا z را وارد کنید : ")
+
+// if(x * y == z) {
+
+//     alert("بله برابر است ")
+// } 
+// else{
+//     alert("خیر برابر نیست")
+// }
+                                     // ______finish part5_____
+
+
+ var userAge = prompt(" لطفا سن خود را وارد کنید : ")
+ var gender = prompt("لطفا جنسیت خود را وارد کنید : (مرد یا زن)")
+ if (gender === "زن" || userAge <= 18){
+alert("شما مجوز استفاده از سایت رو ندارید!!")
+ }
+else {
+alert("به سایت خوش آمدید :) ")    
+}
